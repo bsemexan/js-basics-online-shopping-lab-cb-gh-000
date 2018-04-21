019484df976f00fdad196b352594705885f46f55
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 20:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
   if (!cardNumber){
     return `Sorry, we don't have a credit card on file for you.`;
   }
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   cart = []
 }
