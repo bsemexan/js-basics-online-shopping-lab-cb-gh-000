fd1bd2b1e9df27e2062a5c3f9732ac4592deaf41
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 20:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
       delete cart[i][item];
-      return cart
     }
   }
   return 'That item is not in your cart.'
